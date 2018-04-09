7718553041d58eb59bd8dc61752b4c38c46229c9
PeterChen <747853229@qq.com>
2018-04-09 10:38:58 +0800

    beautify loading bar

--- src/components/Tools/IsLoading.jsx
+++ src/components/Tools/IsLoading.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class IsLoading extends Component {
   render() {
+    const loadingStyleObj = {
+      width: "100%",
+      height: "100%"
+    }
+    const loadingButtonStyleObj = {
+      width: "100%",
+      height: "100%",
+      borderColor: "transparent"
+    }
     return (
-      <div>
-        isLoading
+      <div style={ loadingStyleObj }>
+        <button className="button is-loading" style={ loadingButtonStyleObj }>isLoading...</button>
       </div>
     )
   }
